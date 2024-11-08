fe1cf55003908cfd1d120706178f88c2e5a7ea2f
Kellen James <8484636@ups.com>
2024-11-08 15:45:47 -0500

    feat: add initial project components

---
+++ recipe/src/app/top-image/top-image.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-image',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-image.component.html',
+  styleUrl: './top-image.component.scss'
+})
+export class TopImageComponent {
+
+}
