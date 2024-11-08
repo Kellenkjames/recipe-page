3ec538df1dca2b79ef1daec7a74cf9f59fe19c5e
Kellen James <8484636@ups.com>
2024-11-08 16:19:59 -0500

    feat: add selectors to text-content component

--- recipe/src/app/text-content/text-content.component.ts
+++ recipe/src/app/text-content/text-content.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { IngredientsComponent } from '../ingredients/ingredients.component';
+import { InstructionsComponent } from '../instructions/instructions.component';
+import { NutritionComponent } from '../nutrition/nutrition.component';
+import { PreparationComponent } from '../preparation/preparation.component';
+import { TitleComponent } from '../title/title.component';
 
 @Component({
   selector: 'app-text-content',
   standalone: true,
-  imports: [],
+  imports: [
+    TitleComponent,
+    PreparationComponent,
+    IngredientsComponent,
+    InstructionsComponent,
+    NutritionComponent,
+  ],
   templateUrl: './text-content.component.html',
   styleUrls: ['./text-content.component.scss'],
 })
