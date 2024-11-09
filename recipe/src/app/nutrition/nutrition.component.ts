c8e7e864975c3834329f3bdafd8a02426d7f76b6
Kellen James <8484636@ups.com>
2024-11-09 14:48:35 -0500

    feat: add data properties to nutrition cmp

--- recipe/src/app/nutrition/nutrition.component.ts
+++ recipe/src/app/nutrition/nutrition.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface NutritionValues {
+  type: string;
+  value: string;
+}
+
 @Component({
   selector: 'app-nutrition',
   standalone: true,
@@ -7,4 +12,15 @@ import { Component } from '@angular/core';
   templateUrl: './nutrition.component.html',
   styleUrls: ['./nutrition.component.scss'],
 })
-export class NutritionComponent {}
+export class NutritionComponent {
+  title = 'Nutrition';
+  desc =
+    'The table below shows nutritional values per serving without the additional fillings.';
+
+  nutritionValues: NutritionValues[] = [
+    { type: 'Calories', value: '277kcal' },
+    { type: 'Carbs', value: '0g' },
+    { type: 'Protein', value: '20g' },
+    { type: 'Fat', value: '22g' },
+  ];
+}
