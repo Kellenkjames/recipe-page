dbcdf40295058cd46c280a01dc6f11e8cbef19c1
Kellen James <8484636@ups.com>
2024-11-09 13:17:10 -0500

    feat: add data properties for IngredientsComponent class

--- recipe/src/app/ingredients/ingredients.component.ts
+++ recipe/src/app/ingredients/ingredients.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './ingredients.component.html',
   styleUrls: ['./ingredients.component.scss'],
 })
-export class IngredientsComponent {}
+export class IngredientsComponent {
+  title = 'Ingredients';
+
+  ingredientsList: string[] = [
+    '2-3 large eggs',
+    'Salt, to taste',
+    'Pepper, to taste',
+    '1 tablespoon of butter or oil',
+    'Optional fillings: cheese, diced vegetables, cooked meats, herbs',
+  ];
+}
